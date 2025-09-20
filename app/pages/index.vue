93297237c7ddda8fe900c8369bd40ee416898caf
hao <cutecat8110@gmail.com>
2025-09-20 23:41:16 +0800

    feat:  i18n

---
+++ app/pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <button v-for="locale in locales" @click="setLocale(locale.code)">
+      {{ locale.name }}
+    </button>
+    <NuxtLink :to="$localePath('index')">{{ $t('home') }}</NuxtLink>
+    <NuxtLink :to="$localePath('index', 'en')">Homepage in English</NuxtLink>
+    <NuxtLink :to="$localePath('user-profile')">Route to {{ $t('profile') }}</NuxtLink>
+
+    <h1>{{ $t('welcome') }}</h1>
+  </div>
+</template>
+
+<script setup>
+  const { locales, setLocale } = useI18n()
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,18 +3,33 @@ import tailwindcss from '@tailwindcss/vite'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   // 開發工具
   devtools: { enabled: true },
+
   // 全域 CSS
   css: ['~/assets/css/main.css'],
+
   // TypeScript
   typescript: {
     typeCheck: true,
   },
+
   // 模組
-  modules: ['@nuxt/eslint', '@nuxt/ui'],
+  modules: ['@nuxt/eslint', '@nuxt/ui', '@nuxtjs/i18n'],
+
   // Vite
   vite: {
     plugins: [tailwindcss()],
+  },
+
+  // i18n
+  i18n: {
+    langDir: 'locales/',
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'zh-tw', name: 'Chinese', file: 'zh-tw.json' },
+    ],
   },
 })
